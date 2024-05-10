37f60d62397762e8e8c14c503ab07fab99b93c38
Bea <bea.mondayrocks@gmail.com>
2024-05-11 00:20:22 +0200

    feat: project

---
+++ server/server.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import OpenAI from 'openai'; // Correct import
+import { config } from 'dotenv';
+config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Initialize OpenAI API with the API key
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+console.log(process.env.OPENAI_API_KEY);
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from Bea!'
+  })
+})
+
+app.post('/', async (req, res) => {
+  try {
+    const {prompt} = req.body;
+
+    if (!prompt || typeof prompt !== 'string') {
+      return res.status(400).send({ message: 'Invalid input: Prompt must be a non-empty string.' });
+    }
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    console.log(response.choices[0].message, 'res.choices[0].message in server');
+    
+    res.status(200).send({
+      bot: response.choices[0].message
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Something went wrong', error: error.message });
+  }
+});
+
+
+app.listen(5001, () => console.log('Server started on http://localhost:5001'));
