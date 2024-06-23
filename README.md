6a0553a3ec23a59ee331d765acf5602220011d77
Bea <bea.mondayrocks@gmail.com>
2024-06-23 19:30:31 +0200

    update README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # codex
+
+# how to start the project:
+# cd client
+# npm run dev
+# cd server
+# npm run server
+# Released app: https://codex-one-gamma.vercel.app/
--- server/server.js
+++ server/server.js
@@ -9,10 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Initialize OpenAI API with the API key
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// // Initialize OpenAI API with the API key
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
 console.log(process.env.OPENAI_API_KEY);
 
